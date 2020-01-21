06c2737cefd7d34b7885a9013071217cf6817877
hasauino <oh91@windowslive.com>
2020-01-21 17:44:54 +0100

    fixed bug: last paragraph

--- js/count_characters.js
+++ js/count_characters.js
@@ -4,16 +4,20 @@ var time_bar = document.getElementById("waveform");
 // Options for the observer (which mutations to observe)
 // https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
 const config = { attributes: true, childList: true, subtree: true };
+// whenever the time in the timebar changes, the event is fired, and subtitles
+// are updated
 const observer = new MutationObserver(update_subtitle);
 observer.observe(time_bar, config);
 
+// video window element
 var element = document.getElementById("vjs_video_3");
+
+// create a div, to be added as a child to video elemen
+//this div will hold the captions
 var caption_container = document.createElement("div");
 caption_container.setAttribute("id", "caption_container");
 caption_container.style.cssText = "text-align: center; position:relative; top: -5em;";
 element.appendChild(caption_container);
-
-
 var caption = document.createElement("p");
 caption.setAttribute("id", "caption");
 caption.style.cssText = "display:inline-block; color: white; background-color: rgba(0, 0, 0, 0.85);";
@@ -79,17 +83,17 @@ function update_subtitle(){
         next_parag_text = as_text(paragraphs[index+1]);
         parag_text = parag_text.replace("###", "").concat("<br>").concat(next_parag_text);
     }
-
     $(caption).html(parag_text);
 
-    
+    // below are helper functions-------
+
     // function receives a time frame as string ("HH:MM:SS") , and returns a 
-    // paragraph object
+    // index of corresponding paragraph
     function get_index_of_paragraph(paragraphs, timestr){
         var timeint = timestr2int(timestr);
         var paragraph_index = 0;
         var i;
-        for (i = 0; i< paragraphs.length; i++){
+        for (i = 0; i< paragraphs.length-1; i++){
             var cond = timeint >= timestr2int(start_time(paragraphs[i+1])) ;
             if (!cond){
                 break;}
@@ -104,7 +108,7 @@ function update_subtitle(){
         return paragraph_index;
     }
     
-    // convert time in string format ("HH:MM:SS") to seconds as int (65)
+    // convert time in string format ("HH:MM:SS") to seconds as total seconds (int)
     function timestr2int(timestr){
         var segments = timestr.split(":");
         var weight = 3600;
@@ -115,16 +119,20 @@ function update_subtitle(){
         } 
         return result;
     }
-     
+    
+    // get start time of a paragraph 
     function  start_time(parag_element){
         parag_element;
         return parag_element.getElementsByClassName("exchange--timestamp-value").item(0).innerText;
     }
+
+    // check if paragraph is complete (no splitting)
     function is_complete(text, eol="###"){
         var val = !(text.length >= eol.length && text.substring(text.length-eol.length, text.length) == eol); 
         return !(text.length >= eol.length && text.substring(text.length-eol.length, text.length) == eol);
     }  
     
+    // get text content of a paragraph
     function as_text(parag_element){
         var content = parag_element.getElementsByClassName("exchange--content");
         return $(content).text();
