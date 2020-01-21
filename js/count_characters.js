568091df371f4544eee13f62428890d3937d5fb4
hasauino <oh91@windowslive.com>
2020-01-21 23:44:08 +0100

    fixed requested: last paragraph with ### error

--- js/count_characters.js
+++ js/count_characters.js
@@ -28,23 +28,23 @@ caption_container.appendChild(caption);
 
 
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
     addCounts();
 });
 
-window.addEventListener("keyup", function() {
+window.addEventListener("keyup", function () {
     addCounts();
 });
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     addCounts();
-    setTimeout(function(){ addCounts() }, 200);
+    setTimeout(function () { addCounts() }, 200);
 });
 
 function addCounts() {
     $(".characters-count").remove();
     var paragraphs = getParagraphs();
-    for(var i = 0; i < paragraphs.length; ++i) {
+    for (var i = 0; i < paragraphs.length; ++i) {
         addCount(paragraphs[i]);
     }
 };
@@ -60,7 +60,7 @@ function addCount(obj) {
 
     var count = text.length;
 
-    if(!is_complete(text)) {
+    if (!is_complete(text)) {
         count -= eol.length;
     }
 
@@ -72,66 +72,67 @@ function getParagraphs() {
     return paragraphs;
 }
 // check if paragraph is complete (no splitting)
-function is_complete(text){ 
-    return !(text.length >= eol.length && text.substring(text.length-eol.length, text.length) == eol);
-} 
+function is_complete(text) {
+    return !(text.length >= eol.length && text.substring(text.length - eol.length, text.length) == eol);
+}
 
 
 
-function update_subtitle(){
+function update_subtitle() {
     var current_time = time_bar.getElementsByClassName("timecode text--noselect")[0];
-    var paragraphs =  getParagraphs();
+    var paragraphs = getParagraphs();
     var index = get_index_of_paragraph(paragraphs, current_time.innerText);
     parag_text = as_text(paragraphs[index]);
-    if (!is_complete(parag_text)){
-        next_parag_text = as_text(paragraphs[index+1]);
+    if (!is_complete(parag_text) && index < paragraphs.length - 1) {
+        next_parag_text = as_text(paragraphs[index + 1]);
         parag_text = parag_text.replace(eol, "").concat("<br>").concat(next_parag_text);
     }
-    $(caption).html(parag_text);
+    $(caption).html(parag_text.replace(eol, ""));
 
     // below are helper functions-------
 
     // function receives a time frame as string ("HH:MM:SS") , and returns a 
     // index of corresponding paragraph
-    function get_index_of_paragraph(paragraphs, timestr){
+    function get_index_of_paragraph(paragraphs, timestr) {
         var timeint = timestr2int(timestr);
         var paragraph_index = 0;
         var i;
-        for (i = 0; i< paragraphs.length-1; i++){
-            var cond = timeint >= timestr2int(start_time(paragraphs[i+1])) ;
-            if (!cond){
-                break;}
+        for (i = 0; i < paragraphs.length - 1; i++) {
+            var cond = timeint >= timestr2int(start_time(paragraphs[i + 1]));
+            if (!cond) {
+                break;
+            }
         }
         paragraph_index = i;
-        if (paragraph_index>0){
-            previous_parag_text = as_text(paragraphs[paragraph_index-1]);
-            if(!is_complete(previous_parag_text)){
-                return paragraph_index - 1; 
+        if (paragraph_index > 0) {
+            previous_parag_text = as_text(paragraphs[paragraph_index - 1]);
+            if (!is_complete(previous_parag_text)) {
+                return paragraph_index - 1;
             }
         }
         return paragraph_index;
     }
-    
+
     // convert time in string format ("HH:MM:SS") to seconds as total seconds (int)
-    function timestr2int(timestr){
+    function timestr2int(timestr) {
         var segments = timestr.split(":");
         var weight = 3600;
         var result = 0;
         for (segment of segments) {
-            result += parseInt(segment)*weight;
+            result += parseInt(segment) * weight;
             weight /= 60;
-        } 
+        }
         return result;
     }
-    
+
     // get start time of a paragraph 
-    function  start_time(parag_element){
+    function start_time(parag_element) {
         return parag_element.getElementsByClassName("exchange--timestamp-value").item(0).innerText;
-    } 
-    
+    }
+
     // get text content of a paragraph
-    function as_text(parag_element){
+    function as_text(parag_element) {
         var content = parag_element.getElementsByClassName("exchange--content");
         return $(content).text();
-    }    
+    }
 }
