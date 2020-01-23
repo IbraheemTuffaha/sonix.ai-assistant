affcff0ffae7ce93aefb78748f2150b3ddd9f837
hasauino <oh91@windowslive.com>
2020-01-23 19:32:18 +0100

    requested fixes: moved function outside, call update caption more often

--- js/add_caption.js
+++ js/add_caption.js
@@ -1,14 +1,12 @@
 // Global declarations
 var eol = "###"
 var time_bar = document.getElementById("waveform");
-// Options for the observer (which mutations to observe)
-// https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
-const config = { attributes: true, childList: true, subtree: true };
-// whenever the time in the timebar changes, the event is fired, and subtitles
-// are updated
-const observer = new MutationObserver(update_caption);
-observer.observe(time_bar, config);
+var vid = document.getElementById("vjs_video_3_html5_api");
+vid.ontimeupdate = update_caption;
 
+// if current time is close to the next paragraph by this margin
+// switch to the next paragraph
+var margin = 5
 // video window element
 var element = document.getElementById("vjs_video_3");
 
@@ -16,6 +14,7 @@ var element = document.getElementById("vjs_video_3");
 //this div will hold the captions
 var caption_container = document.createElement("div");
 caption_container.setAttribute("id", "caption_container");
+caption_container.setAttribute("dir", "rtl");
 caption_container.className = "caption-container";
 element.appendChild(caption_container);
 var caption = document.createElement("p");
@@ -41,8 +40,6 @@ function update_caption() {
     var elabsed_bar = $(".ProgressBar-elapsed");
     var elabsed_bar_width = parseFloat(elabsed_bar["0"].style.cssText.replace("width: ", "").replace("%;", "")); //percentage of total width
     var current_time = total_time * elabsed_bar_width;
-
-
     var paragraphs = getParagraphs();
     var index = get_index_of_paragraph(paragraphs, current_time);
     parag_text = as_text(paragraphs[index]);
@@ -51,50 +48,49 @@ function update_caption() {
         parag_text = parag_text.replace(eol, "").concat("<br>").concat(next_parag_text);
     }
     $(caption).html(parag_text.replace(eol, ""));
+}
 
-    // below are helper functions-------
+// helper functions
 
-    // function receives a time frame as string ("HH:MM:SS") , and returns a 
-    // index of corresponding paragraph
-    function get_index_of_paragraph(paragraphs, current_time) {
-        var paragraph_index = 0;
-        var i;
-        for (i = 0; i < paragraphs.length - 1; i++) {
-            var cond = current_time >= start_time(paragraphs[i + 1]);
-            if (!cond) {
-                break;
-            }
+// function receives a time frame as string ("HH:MM:SS") , and returns a 
+// index of corresponding paragraph
+function get_index_of_paragraph(paragraphs, current_time) {
+    var paragraph_index = 0;
+    var i;
+    for (i = 0; i < paragraphs.length - 1; i++) {
+        if (current_time+margin < start_time(paragraphs[i + 1])) {
+            break;
         }
-        paragraph_index = i;
-        if (paragraph_index > 0) {
-            previous_parag_text = as_text(paragraphs[paragraph_index - 1]);
-            if (!is_complete(previous_parag_text)) {
-                return paragraph_index - 1;
-            }
+    }
+    paragraph_index = i;
+    if (paragraph_index > 0) {
+        previous_parag_text = as_text(paragraphs[paragraph_index - 1]);
+        if (!is_complete(previous_parag_text)) {
+            return paragraph_index - 1;
         }
-        return paragraph_index;
     }
+    return paragraph_index;
+}
 
-    // convert time in string format ("HH:MM:SS") to seconds as total seconds (int)
-    function timestr2int(timestr) {
-        var segments = timestr.split(":");
-        var weight = 3600;
-        var result = 0;
-        for (segment of segments) {
-            result += parseInt(segment) * weight;
-            weight /= 60;
-        }
-        return result;
+// convert time in string format ("HH:MM:SS") to seconds as total seconds (int)
+function timestr2int(timestr) {
+    var segments = timestr.split(":");
+    var weight = 3600;
+    var result = 0;
+    for (segment of segments) {
+        result += parseInt(segment) * weight;
+        weight /= 60;
     }
+    return result;
+}
 
-    // get start time of a paragraph 
-    function start_time(parag_element) {
-        return parseFloat(parag_element.dataset.exchangeFrom);
-    }
+// get start time of a paragraph 
+function start_time(parag_element) {
+    return parseFloat(parag_element.dataset.exchangeFrom);
+}
 
-    // get text content of a paragraph
-    function as_text(parag_element) {
-        var content = parag_element.getElementsByClassName("exchange--content");
-        return $(content).text();
-    }
+// get text content of a paragraph
+function as_text(parag_element) {
+    var content = parag_element.getElementsByClassName("exchange--content");
+    return $(content).text();
 }
