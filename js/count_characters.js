90f89ba800f7f816e7ed5316faeb7580f30a0539
hasauino <oh91@windowslive.com>
2020-02-15 00:59:58 +0100

    added option to make count update continously or on play

--- js/count_characters.js
+++ js/count_characters.js
@@ -1,11 +1,61 @@
+var counts_always;
+
+function count_onload() {
+    addCounts();
+};
+
+function count_onkeyup() {
+    addCounts();
+}
+
+function count_onscroll() {
+    addCounts();
+    setTimeout(function () { addCounts() }, 200);
+};
+
+function count_onplay() {
+    addCounts();
+    window.addEventListener("keyup", clearCounts);
+};
+
+
+chrome.storage.local.get(['counts_always'], function (result) {
+    counts_always = result.counts_always;
+    if (counts_always) {
+        window.addEventListener("load", count_onload);
+        window.addEventListener("keyup", count_onkeyup);
+        window.addEventListener('scroll', count_onscroll);
+    }
+});
+
+
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+    if ("counts_always" in changes) {
+        if (!changes["counts_always"].oldValue && changes["counts_always"].newValue) {
+            window.addEventListener("load", count_onload);
+            window.addEventListener("keyup", count_onkeyup);
+            window.addEventListener('scroll', count_onscroll);
+            vid.removeEventListener("play", count_onplay);
+            window.removeEventListener("keyup", clearCounts);
+        }
+        if (changes["counts_always"].oldValue && !changes["counts_always"].newValue) {
+            window.removeEventListener("load", count_onload);
+            window.removeEventListener("keyup", count_onkeyup);
+            window.removeEventListener("scroll", count_onscroll);
+            vid.addEventListener("play", count_onplay);
+            window.addEventListener("keyup", clearCounts);         
+        }
+    }
+
+});
+
+
+
 var eol = "###"
 
 var vid = document.getElementById("vjs_video_3_html5_api");
 
-vid.addEventListener("play", function () {
-    addCounts();
-    window.addEventListener("keyup", clearCounts);
-});
+vid.addEventListener("play", count_onplay);
 
 function addCounts() {
     $(".characters-count").remove();
